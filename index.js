fe775f741c3368c31cbba477009af31620850d1c
Kunal19980 <kunalkashyap1498@gmail.com>
2025-07-02 23:56:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,4 +12,15 @@ console.log(result); // Output: 12
 const addTwoNumbers = (a, b) => a + b;
 
 console.log(addTwoNumbers(10, 15)); // Output: 25
+
+console.log(addTwoNumbers(10, 15)); // Output: 25
+console.log(addTwoNumbers(10, 15)); // Output: 25
+console.log(addTwoNumbers(10, 15)); // Output: 25
+console.log(addTwoNumbers(10, 15)); // Output: 25
+console.log(addTwoNumbers(10, 15)); // Output: 25
+console.log(addTwoNumbers(10, 15)); // Output: 25
+console.log(addTwoNumbers(10, 15)); // Output: 25
+console.log(addTwoNumbers(10, 15)); // Output: 25
+console.log(addTwoNumbers(10, 15)); // Output: 25
  
+
