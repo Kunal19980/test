eeba0a6527fd926437b237dc7740a9d393b34bd6
Kunal19980 <kunalkashyap1498@gmail.com>
2025-07-02 23:52:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ function addTwoNumbers(a, b) {
 }
 
 // Example usage:
+
 console.log("change occures");
 const result = addTwoNumbers(5, 7);
 console.log(result); // Output: 12
 
 
-// const addTwoNumbers = (a, b) => a + b;
+const addTwoNumbers = (a, b) => a + b;
 
-// console.log(addTwoNumbers(10, 15)); // Output: 25
+console.log(addTwoNumbers(10, 15)); // Output: 25
  
