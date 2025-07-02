b76d9eac4246ae7032248a47c511ee2b753312c8
kunal <kunalkashyap1498@gmail.com>
2025-07-02 23:25:44 +0530

    my second commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const result = addTwoNumbers(5, 7);
 console.log(result); // Output: 12
 
 
-const addTwoNumbers = (a, b) => a + b;
+// const addTwoNumbers = (a, b) => a + b;
 
-console.log(addTwoNumbers(10, 15)); // Output: 25
+// console.log(addTwoNumbers(10, 15)); // Output: 25
  
