8006338b5cd9b35f00afc158eede4f1769efaa50
kunal <kunalkashyap1498@gmail.com>
2025-07-02 23:13:27 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+// Example usage:
+const result = addTwoNumbers(5, 7);
+console.log(result); // Output: 12
+
+
+const addTwoNumbers = (a, b) => a + b;
+
+console.log(addTwoNumbers(10, 15)); // Output: 25
+ 
